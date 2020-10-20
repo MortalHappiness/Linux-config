ecf1502df08d37b3f2de99b6dfdebb16d9a686a7
MortalHappiness <b07901069@ntu.edu.tw>
2020-10-20 22:55:23 +0800

    Update

--- X11-config/utils/touchpad.sh
+++ X11-config/utils/touchpad.sh
@@ -7,8 +7,8 @@ state=$(xinput list-props "$touchpad" | grep 'Device Enabled' | cut -f3)
 
 if [ "$state" == 1 ]; then
     xinput disable "$touchpad"
-    notify-send -i $HOME/.config/sxhkd/images/Touchpad_Disabled.png "Touchpad" "Touchpad Disabled"
+    notify-send -i $HOME/utils/images/Touchpad_Disabled.png "Touchpad" "Touchpad Disabled"
 else
     xinput enable "$touchpad"
-    notify-send -i $HOME/.config/sxhkd/images/Touchpad.png "Touchpad" "Touchpad Enabled"
+    notify-send -i $HOME/utils/images/Touchpad.png "Touchpad" "Touchpad Enabled"
 fi
