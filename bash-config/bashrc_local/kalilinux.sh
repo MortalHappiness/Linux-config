9c9cfe5548909c4a345be80174120ed4b06d5a0b
MortalHappiness <b07901069@ntu.edu.tw>
2020-10-23 02:10:42 +0800

    Update

--- bash-config/bashrc_local/kalilinux.sh
+++ bash-config/bashrc_local/kalilinux.sh
@@ -2,3 +2,8 @@ alias pip=pip3
 alias python=python3
 
 export PATH="$HOME/bin:$PATH"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
