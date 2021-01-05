1177cf1e9eac2f62a28586b3413105a9d7f4e380
MortalHappiness <b07901069@ntu.edu.tw>
2021-01-05 15:01:15 +0800

    Update

--- bash-config/bashrc_local/kalilinux.sh
+++ bash-config/bashrc_local/kalilinux.sh
@@ -7,3 +7,7 @@ export PATH="$PATH:$HOME/bin"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# gem
+
+export PATH="$PATH:$HOME/.gem/ruby/2.7.0/bin"
