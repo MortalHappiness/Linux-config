506295640a6356fb9c58b44d7a5e43a422e93389
MortalHappiness <b07901069@ntu.edu.tw>
2022-01-31 03:21:34 +0800

    Update fishconfig

--- fish-config/omf/init.fish
+++ fish-config/omf/init.fish
@@ -1,2 +1,28 @@
 set -xg EDITOR vim
 umask 022
+
+set fish_greeting
+
+set -l gray 75715E
+set -l red FF0000
+set -l pink FF005F
+set -l orange FF8700
+set -l yellow D7D75F
+set -l green 87D700
+set -l blue 5FD7FF
+set -l purple AF87FF
+
+set -g fish_color_normal normal
+set -g fish_color_command $blue
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $purple
+set -g fish_color_end normal
+set -g fish_color_error $red
+set -g fish_color_param normal
+set -g fish_color_comment $gray
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_operator $pink
+set -g fish_color_escape $yellow
+set -g fish_color_autosuggestion $gray
+set -g fish_color_search_match while --bold --background=brblack
--- fish-config/omf/key_bindings.fish
+++ fish-config/omf/key_bindings.fish
@@ -1,2 +1,3 @@
+set -g fish_key_bindings fish_vi_key_bindings
 fish_default_key_bindings -M insert
 fish_vi_key_bindings --no-erase insert
